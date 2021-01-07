5685c3e60640b171c70856cd99988f5d0da228ab
stelson <sthelmo@gmail.com>
2021-01-07 21:25:51 +0100

    Reverse array

--- chapter4.js
+++ chapter4.js
@@ -26,3 +26,31 @@ console.log(sum(range(1, 10)));
 console.log(sum(range(1, 10, -1)));
 console.log(sum(range(1, 10, 3)));
 console.log(sum(range(10, 100, 2)));
+
+
+console.log('Reverse array');
+///===========2===========
+
+function reverseArray(array) {
+  var reversedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    reversedArray.push(array[array.length - 1 - i])
+  }
+  return reversedArray;
+}
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
