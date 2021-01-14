1b626fde908ba7513110d4ec434363ca52d79175
stelson <sthelmo@gmail.com>
2021-01-14 21:55:10 +0100

    5.2 your own function

--- chapter5.js
+++ chapter5.js
@@ -5,3 +5,15 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 let newArray = arrays.reduce((flatArray, next) => flatArray.concat(next), []);
 console.log(newArray);
 // → [1, 2, 3, 4, 5, 6]
+
+
+///===========2===========
+function loop(start, test, update, body) {
+    for(let i = start; test(i); i = update(i))
+        body(i);
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
