0aa31a6d649950482720dfdc11f298e84f4e9fa0
stelson <sthelmo@gmail.com>
2021-01-16 21:32:18 +0100

    5.4 scripts

--- chapter5.js
+++ chapter5.js
@@ -1,3 +1,4 @@
+require('./scripts.js')
 ///===========1===========
 
 let arrays = [[1, 2, 3], [4, 5], [6]];
@@ -44,3 +45,47 @@ console.log(every2([2, 4, 16], n => n < 10));
 // → false
 console.log(every2([], n => n < 10));
 // → true
+
+
+///===========4===========
+function countBy(items, groupName) {
+    let counts = [];
+    for (let item of items) {
+        let name = groupName(item);
+        let known = counts.findIndex(c => c.name == name);
+        if (known == -1) {
+            counts.push({name, count: 1});
+        } else {
+            counts[known].count++;
+        }
+    }
+    return counts;
+}
+
+function characterScript(code) {
+    for (let script of SCRIPTS) {
+        if (script.ranges.some(([from, to]) => {
+            return code >= from && code < to;
+        })) {
+            return script;
+        }
+    }
+    return null;
+}
+
+function dominantDirection(text) {
+    let counted = countBy(text, char => {
+        let script = characterScript(char.codePointAt(0));
+        return script ? script.direction : "none";
+    }).filter(({name}) => name != "none");
+
+    if (counted.length == 0) 
+        return "ltr";
+    
+    return counted.reduce((a, b) => a.count > b.count ? a : b).name;
+}
+  
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
