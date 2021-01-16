123b79c423fa5f31660e3b72c6d8de66544ff7a7
stelson <sthelmo@gmail.com>
2021-01-16 21:12:21 +0100

    5.3 "every" function

--- chapter5.js
+++ chapter5.js
@@ -17,3 +17,30 @@ loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
 // → 2
 // → 1
+
+
+///===========3===========
+function every1(array, test) {
+    for (let element of array) {
+        if (!test(element)) 
+            return false;
+    }
+    return true;
+}
+
+function every2(array, test) {
+    return !array.some(el => !test(el));
+}
+  
+console.log(every1([1, 3, 5], n => n < 10));
+// → true
+console.log(every1([2, 4, 16], n => n < 10));
+// → false
+console.log(every1([], n => n < 10));
+// → true
+console.log(every2([1, 3, 5], n => n < 10));
+// → true
+console.log(every2([2, 4, 16], n => n < 10));
+// → false
+console.log(every2([], n => n < 10));
+// → true
