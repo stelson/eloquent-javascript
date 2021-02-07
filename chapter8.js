8d15acd32c3a5f436401f01797b4fa3a2dd9163b
stelson <sthelmo@gmail.com>
2021-02-07 18:41:33 +0100

    chapter 8.2

--- chapter8.js
+++ chapter8.js
@@ -22,3 +22,45 @@ function reliableMultiply(a, b) {
 
 console.log(reliableMultiply(8, 8));
 // → 64
+
+
+//////////////////////////////////
+
+
+const box = {
+    locked: true,
+    unlock() { this.locked = false; },
+    lock() { this.locked = true;  },
+    _content: [],
+    get content() {
+      if (this.locked) throw new Error("Locked!");
+      return this._content;
+    }
+  };
+  
+  function withBoxUnlocked(body) {
+    if(box.locked) {
+        box.unlock();
+        try {
+          return body();
+        } finally {
+          box.lock();
+        }
+    } else {
+        return body();
+    }
+  }
+  
+  withBoxUnlocked(function() {
+    box.content.push("gold piece");
+  });
+  
+  try {
+    withBoxUnlocked(function() {
+      throw new Error("Pirates on the horizon! Abort!");
+    });
+  } catch (e) {
+    console.log("Error raised: " + e);
+  }
+  console.log(box.locked);
+  // → true
