7603b4edda72db392096456b8cb0c4b61c9a07a9
stelson <sthelmo@gmail.com>
2021-02-13 22:31:56 +0100

    chapter 9.3

--- chapter9.js
+++ chapter9.js
@@ -39,3 +39,33 @@ function verify(regexp, yes, no) {
         console.log(`Unexpected match for '${str}'`);
     }
 }
+
+
+//////////////////////////////////////////////////////////
+
+
+let text = "'I'm the cook,' he said, 'it's my job.'";
+// Change this call.
+console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));
+// → "I'm the cook," he said, "it's my job."
+
+
+////////////////////////////////////////////////////////////
+
+
+// Fill in this regular expression.
+let number = /^[+\-]?(\d+(\.\d*)?|\.\d+)([eE][+\-]?\d+)?$/;
+
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+                 "1.3e2", "1E-4", "1e+12"]) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+  }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+                 ".5.", "1f5", "."]) {
+  if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`);
+  }
+}
